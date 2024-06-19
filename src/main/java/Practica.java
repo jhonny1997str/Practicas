145710bc17815167df49fc51335f424a0d7fb85f
jhonny1997str <167597745+jhonny1997str@users.noreply.github.com>
2024-06-18 21:47:33 -0500

    Matriz dinamica clase objeto

--- src/main/java/Practica.java
+++ src/main/java/Practica.java
@@ -1,19 +1,74 @@
-import javax.swing.*;
+import java.util.Scanner;
 
-public class Practica extends JFrame {
-    private JLabel label1;
-    public Practica(){
-        setLayout(null);
-        label1 = new JLabel("HOLA MUNDO");
-        label1.setBounds(0,0,200,300);
-        add(label1);
+public class Practica {
+    private int filas;
+    private int columnas;
+    private int[][] numeros;
+
+    // Constructor
+    public Practica(int filas, int columnas) {
+        this.filas = filas;
+        this.columnas = columnas;
+        this.numeros = new int[filas][columnas]; // Inicializamos el array de números con el tamaño del vector
+    }
+
+    // Métodos getter
+    public int getFilas() {
+        return filas;
+    }
+
+    public int getColumnas() {
+        return columnas;
+    }
+
+    // Métodos setter
+    public void setVector(int filas, int columnas) {
+        this.filas = filas;
+        this.columnas = columnas;
+        this.numeros = new int[filas][columnas]; // Actualizamos el tamaño del array de números
+    }
+
+    // Algoritmo para llenar el vector
+    public void algoritmo() {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas;j++){
+                System.out.println("Ingrese el elemento de la posicion " + (i + 1) + "," + (j + 1) + " :");
+                numeros[i][j] = scanner.nextInt();
+
+            }
+
+        }
+    }
+
+    // Método para mostrar información del vector
+    public void mostrarinfo() {
+        System.out.println("La Matriz creada  es: ");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas;j++){
+                System.out.print("[" + numeros[i][j] + "]");
+
+            }
+            System.out.println();
+
+        }
+
     }
 
     public static void main(String[] args) {
-        Practica practica1 = new Practica();
-        practica1.setBounds(100,100,1000,1000);
-        practica1.setVisible(true);
-        practica1.setLocationRelativeTo(null);
-        practica1.setResizable(false);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de filas : ");
+        int filas = scanner.nextInt();
+        System.out.println("Ingrese la cantidad de columnas : ");
+        int columnas = scanner.nextInt();
+
+        // Crear una instancia de Practica con el tamaño del vector
+        Practica practica = new Practica(filas, columnas);
+
+        // Llenar el vector con el algoritmo
+        practica.algoritmo();
+
+        // Mostrar la información del vector
+        practica.mostrarinfo();
     }
 }
